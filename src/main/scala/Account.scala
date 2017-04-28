eb9bc59d8c777a481c3a8df837e98f91da88a2f9
John Hunter <john@push-it-forward.com>
2017-04-28 10:38:59 -0700

    fixed thread locking issue

--- src/main/scala/Account.scala
+++ src/main/scala/Account.scala
@@ -1,14 +1,23 @@
 /**
   * Created by jhunter on 4/27/17.
   */
-class Account(private var amount: Int = 0) {
+class Account(private var amount: Int = 0, private val uid: Int) {
+
+  def getUniqueId() ={
+    uid
+  }
+
   def transfer(target: Account, n: Int) = {
+    if (this.uid < target.uid) this.lockAndTransfer(target, n)
+    else target.lockAndTransfer(this, -n)
+  }
+
+  private def lockAndTransfer(target: Account, n: Int) = {
     this.synchronized {
       target.synchronized {
         this.amount -= n
-        this.amount += n
+        target.amount += n
       }
-
     }
   }
 }
--- src/main/scala/AccountConcurrency.scala
+++ src/main/scala/AccountConcurrency.scala
@@ -4,8 +4,8 @@
 object AccountConcurrency {
 
   def main(args: Array[String]): Unit = {
-    val a1 = new Account(500000)
-    val a2 = new Account(700000)
+    val a1 = new Account(500000, 1)
+    val a2 = new Account(700000, 2)
     val t = startThread(a1, a2, 150000)
     val s = startThread(a2, a1, 150000)
     t.join()
